2cb98c61284b5aebfe8563e9247cba61a89ecd3d
AlexeyPetroff <sidr.alexey@gmail.com>
2017-05-30 18:01:43 +0200

    Full assignment

--- src/program.py
+++ src/program.py
@@ -1,12 +1,11 @@
-dbooks = {"Part1":8, "Part2":8, "Part3":8, "Part4":8, "Part5":8}
+books_prices = {"Part1": 8, "Part2": 8, "Part3": 8, "Part4": 8, "Part5": 8}
 
 
 def get_price(name):
-    return dbooks[name]
+    return books_prices[name]
 
 
 class Cart:
-
     cart = {}
 
     def __init__(self):
@@ -19,11 +18,33 @@ class Cart:
         return sum(self.cart.values())
 
     def count_price(self):
+        cart_temp = self.cart
         names_of_books = self.cart.keys()
         price = 0
-
-        for name in names_of_books:
-            price += dbooks[name]*self.cart[name]
-        if len(names_of_books) > self.count_all_items():
-            price = price * 0.95
+        sets = []
+        for piece in names_of_books:
+            while cart_temp[piece] > 0:
+                create_new = True
+                for one_set in sets:
+                    if piece not in one_set:
+                        one_set.append(piece)
+                        create_new = False
+                        break
+                if create_new:
+                    new_set = [piece]
+                    sets.append(new_set)
+                cart_temp[piece] -= 1
+        for one_set in sets:
+            set_price = 0
+            for book in one_set:
+                set_price += books_prices[book]
+            if len(one_set) == 2:
+                set_price = set_price * 0.95
+            elif len(one_set) == 3:
+                set_price = set_price * 0.9
+            elif len(one_set) == 4:
+                set_price = set_price * 0.8
+            elif len(one_set) == 5:
+                set_price = set_price * 0.75
+            price += set_price
         return price
--- tests/example.py
+++ tests/example.py
@@ -1,11 +1,10 @@
 import unittest
+
 from src.program import Cart, get_price
 
 
 class ExampleTest(unittest.TestCase):
-
     def test1_get_price(self):
-        c = Cart()
         price = get_price('Part1')
         self.assertEqual(price, 8)
 
@@ -20,9 +19,9 @@ class ExampleTest(unittest.TestCase):
 
     def test4_add_diff_books(self):
         c = Cart()
-        c.add_to_cart('Part1',1)
+        c.add_to_cart('Part1', 1)
         c.add_to_cart('Part2', 3)
-        self.assertEquals(c.count_all_items(),4)
+        self.assertEquals(c.count_all_items(), 4)
 
     def test5_get_price(self):
         c = Cart()
@@ -44,7 +43,80 @@ class ExampleTest(unittest.TestCase):
         c = Cart()
         c.add_to_cart('Part1', 2)
         c.add_to_cart('Part2', 1)
-        self.assertEquals(c.count_price(), 15.2+8)
+        self.assertEquals(c.count_price(), 15.2 + 8)
+
+    def test9_get_price_for_p1_p1_p2_p2(self):
+        c = Cart()
+        c.add_to_cart('Part1', 2)
+        c.add_to_cart('Part2', 2)
+        self.assertEquals(c.count_price(), 15.2 + 15.2)
+
+    def test10_get_price_for_p1_p2_p3(self):
+        c = Cart()
+        c.add_to_cart('Part1', 1)
+        c.add_to_cart('Part2', 1)
+        c.add_to_cart('Part3', 1)
+        self.assertEquals(c.count_price(), 8 * 3 * 0.9)
+
+    def test11_get_price_for_p1_p1_p2_p3(self):
+        c = Cart()
+        c.add_to_cart('Part1', 2)
+        c.add_to_cart('Part2', 1)
+        c.add_to_cart('Part3', 1)
+        self.assertEquals(c.count_price(), 8 * 3 * 0.9 + 8)
+
+    def test12_get_price_for_p1_p2_p3_p4(self):
+        c = Cart()
+        c.add_to_cart('Part1', 1)
+        c.add_to_cart('Part2', 1)
+        c.add_to_cart('Part3', 1)
+        c.add_to_cart('Part4', 1)
+        self.assertEquals(c.count_price(), 8 * 4 * 0.8)
+
+    def test13_get_price_for_p1_p2_p2_p3_p4(self):
+        c = Cart()
+        c.add_to_cart('Part1', 1)
+        c.add_to_cart('Part2', 2)
+        c.add_to_cart('Part3', 1)
+        c.add_to_cart('Part4', 1)
+        self.assertEquals(c.count_price(), 8 * 4 * 0.8 + 8)
+
+    def test14_get_price_for_p1_p2_p3_p4_p5(self):
+        c = Cart()
+        c.add_to_cart('Part1', 1)
+        c.add_to_cart('Part2', 1)
+        c.add_to_cart('Part3', 1)
+        c.add_to_cart('Part4', 1)
+        c.add_to_cart('Part5', 1)
+        self.assertEquals(c.count_price(), 8 * 5 * 0.75)
+
+    def test15_get_price_for_two_full_sets(self):
+        c = Cart()
+        c.add_to_cart('Part1', 2)
+        c.add_to_cart('Part2', 2)
+        c.add_to_cart('Part3', 2)
+        c.add_to_cart('Part4', 2)
+        c.add_to_cart('Part5', 2)
+        self.assertEquals(c.count_price(), 8 * 5 * 0.75 * 2)
+
+    def test16_get_price_for_full_and_almost(self):
+        c = Cart()
+        c.add_to_cart('Part1', 2)
+        c.add_to_cart('Part2', 2)
+        c.add_to_cart('Part3', 2)
+        c.add_to_cart('Part4', 2)
+        c.add_to_cart('Part5', 1)
+        self.assertEquals(c.count_price(), 8 * 5 * 0.75 + 8 * 4 * 0.8)
+
+    def test17_example_from_readme(self):
+        c = Cart()
+        c.add_to_cart('Part1', 2)
+        c.add_to_cart('Part2', 2)
+        c.add_to_cart('Part3', 2)
+        c.add_to_cart('Part4', 1)
+        c.add_to_cart('Part5', 1)
+        self.assertEquals(c.count_price(), 51.6)
+
 
 if __name__ == '__main__':
     unittest.main()
